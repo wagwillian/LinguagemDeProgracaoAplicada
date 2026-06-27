af28348d11e22d8d5598b3c67c8cd528f632d67e
Wagner Willian <wwop09@gmail.com>
2026-06-27 17:12:23 -0300

    Adiciona Arquivos de config e sprites, e cria os principais loodo jogo

---
+++ main.py
@@ -0,0 +1,67 @@
+import pygame
+from pygame.examples.aliens import Player
+
+from sprites import *
+from config import *
+import sys
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def new(self):
+        # Inicializa o jogo
+        self.playing = True
+
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.blocks = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates()
+        self.attacks = pygame.sprite.LayeredUpdates()
+
+        self.player = Player(self, 1, 2)
+
+    def events(self):
+        #game loop events - define os eventos
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+
+    def update(self):
+        #atualiza os quadros
+        self.all_sprites.update()
+
+    def draw(self):
+        #game loop draw - desenha os quadros
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
+
+    def main(self):
+        #game loop - define o loop principal
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        self.running = False
+
+    def game_over(self):
+        pass
+
+    def intro_screen(self):
+        pass
+g = Game()
+g.intro_screen()
+g.new()
+while g.running:
+    g.main()
+    g.game_over()
+
+pygame.quit()
+sys.exit()
+
